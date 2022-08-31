067017096b110eaf8f04ec968031fe4d2ca8cf9e
Jvictorcam <jvictorac@proton.me>
2022-08-31 19:44:31 -0300

    implementing else and session warn. Bugs!

--- ImgFilter/html/receiver.php
+++ ImgFilter/html/receiver.php
@@ -11,8 +11,15 @@ $filetype = getExt($_FILES['usrfile']['type']);
 if(in_array($filetype, array('jpeg', 'png', 'jpg'))){
     $filename = 'image'.md5(time().rand(1, 1000)) . '.jpg';
     move_uploaded_file($_FILES['usrfile'], '/files/' . $filename);
-
+    echo '<pre>';
     echo 'File Uploaded Succesfully';
+    print_r($_FILES['usrfile']); 
+}
+
+else{
+    $_SESSION['warn'] = 'Invalid Format, please select a jpg/png file';
+    header("Location: index.php");
+    exit();
 }
 
 ?>
