03d244574199f46564584a082e6091477bdb050f
Jvictorcam <jvictorac@proton.me>
2022-08-31 19:53:15 -0300

    fixed bugs and files folder added

--- ImgFilter/html/receiver.php
+++ ImgFilter/html/receiver.php
@@ -10,7 +10,7 @@ $filetype = getExt($_FILES['usrfile']['type']);
 
 if(in_array($filetype, array('jpeg', 'png', 'jpg'))){
     $filename = 'image'.md5(time().rand(1, 1000)) . '.jpg';
-    move_uploaded_file($_FILES['usrfile'], '/files/' . $filename);
+    move_uploaded_file($_FILES['usrfile']['tmp_name'], "files/$filename");
     echo '<pre>';
     echo 'File Uploaded Succesfully';
     print_r($_FILES['usrfile']); 
