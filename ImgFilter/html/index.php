b6b117343867632e4c22ddf917dde569a4a456c8
Jvictorcam <jvictorac@proton.me>
2022-09-01 18:14:51 -0300

    Treating Session warn if the file isnt a image file

--- ImgFilter/html/index.php
+++ ImgFilter/html/index.php
@@ -1,6 +1,14 @@
 <?php session_start(); ?>
+
 <form method="POST" action="receiver.php" enctype="multipart/form-data">
     <input type="file" name="usrfile" />
     <input type="submit" value="Send" />
+    <br/>
 </form>
+
+<?php 
+echo $_SESSION['warn'] ?? '';
+session_unset();
+?>
+
 
