d2f83ffd9c4f9b2769eced86c8993440809ae320
Jvictorcam <jvictorac@proton.me>
2022-09-01 14:16:42 -0300

    Form using cookies to rememberme function/First Commit

---
+++ UsingCookies/html/index.php
@@ -0,0 +1,27 @@
+<form method="POST" action="receiver.php">
+ 
+    <label>
+        Name:
+        <br/>
+        <input type="text" name="usrname"/>
+    </label>
+    <br/>
+    <br/>
+
+    <label>
+        E-mail:
+        <br/>
+        <input type="text" name="usremail"/>
+    <label>
+    <br/>
+    <br/>
+
+    <label>
+        Password:
+        <br/>
+        <input type="password" name="usrpasswd"/>
+    </label>
+    <br/>
+    <br/>
+    
+</form>
